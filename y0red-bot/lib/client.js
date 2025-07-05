b81ba1b3657247bca8e3bf7c4dd01e309441cf69
Y0RED <yayele88@gmail.com>
2025-07-05 16:07:54 +0300

    a

--- y0red-bot/lib/client.js
+++ y0red-bot/lib/client.js
@@ -54,7 +54,7 @@ export class TelegramApiClient {
       game_short_name,
       reply_markup: {
         inline_keyboard: [
-          [{ text: button_text, callback_game: `play_${game_short_name}`,  url: "https://t.me/PlayGroundGamesBot/PGGC"}]
+          [{ text: button_text,  url: "https://t.me/PlayGroundGamesBot/PGGC"}]
         ]
       }
     });
