34caccd4f1f549aca7af4c431c1ec5ed62ee9c2e
Y0RED <yayele88@gmail.com>
2025-07-05 19:06:52 +0300

    asd

--- y0red-bot/lib/client.js
+++ y0red-bot/lib/client.js
@@ -51,9 +51,8 @@ export class TelegramApiClient {
   requestContact({ chat_id, text = 'Share Phone Number', button_text = 'Share', request_contact }) {
     return this._apiCall('sendMessage', {
       chat_id,
-      text,
       reply_markup: {
-        inline_keyboard_button: [
+        inline_keyboard: [
           [{ text: "share",  request_contact }]
         ]
       }
